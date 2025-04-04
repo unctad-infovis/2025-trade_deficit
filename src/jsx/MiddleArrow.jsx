aaa1fb0c09777723e56e00da291d7f112a989cb0
Teemo Tebest <teemo.tebest@gmail.com>
2025-04-04 17:01:28 +0200

    Fixed url

--- src/jsx/MiddleArrow.jsx
+++ src/jsx/MiddleArrow.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MiddleArrow() {
   return (
     <div className="middle-arrow" aria-hidden="true">
-      <img src="../assets/img/righarrow.png" alt="" />
+      <img src="./../assets/img/righarrow.png" alt="" />
     </div>
   );
 }
